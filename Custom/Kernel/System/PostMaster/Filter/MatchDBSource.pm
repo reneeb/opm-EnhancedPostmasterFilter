c68a72b450da1780c68794e7697053c28fd0c0dd
reneeb <info@perl-services.de>
2016-09-28 12:15:21 +0200

    changes for OTRS5

--- Custom/Kernel/System/PostMaster/Filter/MatchDBSource.pm
+++ Custom/Kernel/System/PostMaster/Filter/MatchDBSource.pm
@@ -1,6 +1,5 @@
 # --
-# Kernel/System/PostMaster/Filter/MatchDBSource.pm - sub part of PostMaster.pm
-# Copyright (C) 2001-2014 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2016 OTRS AG, http://otrs.com/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -38,8 +37,10 @@ sub Run {
     # check needed stuff
     for (qw(JobConfig GetParam)) {
         if ( !$Param{$_} ) {
-            $Kernel::OM->Get('Kernel::System::Log')
-                ->Log( Priority => 'error', Message => "Need $_!" );
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $_!"
+            );
             return;
         }
     }
