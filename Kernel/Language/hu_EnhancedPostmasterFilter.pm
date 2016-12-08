d4535ed141610947550ea1f2db5275a5a57f505a
Balázs Úr <urbalazs@gmail.com>
2016-12-08 15:54:32 +0100

    Added Hungarian translation, synced with upstream

--- Kernel/Language/hu_EnhancedPostmasterFilter.pm
+++ Kernel/Language/hu_EnhancedPostmasterFilter.pm
@@ -1,13 +1,14 @@
 # --
-# Kernel/Language/de_EnhancedPostmasterFilter.pm - the German translation for EnhancedPostmasterFilter
+# Kernel/Language/hu_EnhancedPostmasterFilter.pm - the Hungarian translation for EnhancedPostmasterFilter
 # Copyright (C) 2015 - 2016 Perl-Services, http://www.perl-services.de
+# Copyright (C) 2016 Balázs Úr, http://www.otrs-megoldasok.hu
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Language::de_EnhancedPostmasterFilter;
+package Kernel::Language::hu_EnhancedPostmasterFilter;
 
 use strict;
 use warnings;
@@ -20,7 +21,7 @@ sub Data {
     my $Lang = $Self->{Translation} || {};
 
     $Lang->{'You can also use \'named captures\' ((?<groupname>)) and use the names in the \'Set\' action ([**\\groupname**]). (e.g. Regexp: Server: (?<server>\\w+), Set action [**\\server**]). A matched EMAILADDRESS has the name \'email\'.'} =
-        'Sie können auch benannte Treffer ((?<groupname>)) verwenden und die Namen in der \'Set\' Aktion ([**\\\groupname**]) verwenden (z.B. Regulärer Ausdruck: Server: (?<server>\\w+), Set Aktion [**\\server**]). Eine gefundene EMAILADDRESS hat den Namen \'email\'.';
+        'Használhat „elnevezett elfogásokat” is ((?<csoportnev>)), és használhatja a neveket a „Beállítás” műveletben ([**\\csoportnev**]). (Például reguláris kifejezésnél: Kiszolgáló: (?<kiszolgalo>\\w+), Beállítás művelet [**\\kiszolgalo**]). Egy megtalált EMAILCÍM az „email” nevet fogja kapni.';
 }
 
 1;
