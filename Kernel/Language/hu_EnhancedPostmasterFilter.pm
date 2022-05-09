6f55764155bb34adafcaa7f19b1580a1c2bc4dda
reneeb <info@perl-services.de>
2022-05-09 11:02:13 +0200

    Ran bump-copyright-year.pl

--- Kernel/Language/hu_EnhancedPostmasterFilter.pm
+++ Kernel/Language/hu_EnhancedPostmasterFilter.pm
@@ -1,6 +1,6 @@
 # --
 # Kernel/Language/hu_EnhancedPostmasterFilter.pm - the Hungarian translation for EnhancedPostmasterFilter
-# Copyright (C) 2015 - 2016 Perl-Services, http://www.perl-services.de
+# Copyright (C) 2015 - 2022 Perl-Services, https://www.perl-services.de
 # Copyright (C) 2016 Balázs Úr, http://www.otrs-megoldasok.hu
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
